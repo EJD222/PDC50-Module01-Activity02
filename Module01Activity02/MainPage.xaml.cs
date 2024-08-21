603c2d5b801575b504f7e7fb46e313e823a47cbd
EJD222 <eljdiaz500@gmail.com>
2024-08-21 18:20:36 +0800

    Add Module01-Activity02 Files

---
+++ Module01Activity02/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿namespace Module01Activity02
+{
+    public partial class MainPage : ContentPage
+    {
+        public string PageTitle { get; set; }
+        public Color ColorBlack { get; set; }
+        public Color ColorWhite { get; set; }
+        public FontAttributes BoldText { get; set; }
+        public double Heading1 { get; set; }
+        public double Heading2 { get; set; }
+        public double Text1 { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+
+            PageTitle = "Hello, MAUI!";
+            ColorWhite = Colors.White;
+            ColorBlack = Colors.Black;
+            BoldText = FontAttributes.Bold;
+            Heading1 = 50;
+            Heading2 = 22;
+            Text1 = 16;
+
+            this.BindingContext = this;
+        }
+
+        private void OnChangedLabelNameStatic(object sender, EventArgs e)
+        {
+            this.Resources["StaticUsername"] = "Time to TWICE";
+        }
+        private void OnChangedLabelNameDynamic(object sender, EventArgs e)
+        {
+            this.Resources["DynamicUsername"] = "Im Nayeon";
+        }
+        private void OnChangedBackgroundColorToLightGreen(object sender, EventArgs e)
+        {
+            this.Resources["LocalBackgroundColor"] = Color.FromArgb("#A3D1CC");
+        }
+        private void OnChangedBackgroundColorToPink(object sender, EventArgs e)
+        {
+            this.Resources["LocalBackgroundColor"] = Colors.Pink;
+        }
+        private void OnChangedBackgroundColorToLightBlue(object sender, EventArgs e)
+        {
+            this.Resources["LocalBackgroundColor"] = Colors.LightBlue;
+        }
+        private void OnChangedBackgroundColorToBlack(object sender, EventArgs e)
+        {
+            this.Resources["LocalBackgroundColor"] = Colors.Black;
+        }
+        private void OnChangeBoxViewColor(object sender, EventArgs e)
+        {
+            this.Resources["DynamicBoxColor"] = Color.FromArgb("#F3991A");
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+# Module 01 - Activity 02 (PDC50-LAB)
+
+    Name of Student: Elmalia Jane S. Diaz
+    Year and Section: BSIT4A
+    Activity Title: Module 01 - Activity 02
+    Date Submitted: August 21, 2024
+
+### Source Code
+- [MainPage.xaml](PDC50-Module01-Activity02/Module01Activity02/MainPage.xaml)
+- [MainPage.xaml.cs](PDC50-Module01-Activity02/Module01Activity02/MainPage.xaml.cs)
+- [CurrentDateExtension.cs](PDC50-Module01-Activity02/Module01Activity02/CurrentDateExtension.cs)
+
+## App
+### Output Screenshots
+<p align="center">
+    <img src="Screenshots/App-1.png" alt="Add-1" width="270" height="585">
+    <img src="Screenshots/App-2.png" alt="Add-1" width="270" height="585">
+</p>
